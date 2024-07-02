7645b7b8024a4f8d86b0642b93a14b15f1077b05
lexahu <127268653+lexahu@users.noreply.github.com>
2024-07-02 18:50:32 -0400

    Ruby 3 upgrades (#1)
    * Updates to support Ruby 3.x
    * Switch to github actions
    * Test against of Ruby 2.7 and 3.x and Rails 6.0 - 7.1

--- lib/cequel/version.rb
+++ lib/cequel/version.rb
@@ -1,5 +1,10 @@
 # -*- encoding : utf-8 -*-
 module Cequel
-  # The current version of the library
-  VERSION = '3.2.1'
+  ##
+  # ART19 maintains a fork with patches applied on top of the upstream gem.
+  # We publish our fork with a revision number appended to the upstream version.
+  #
+  # @return [String] the ART19 revision number
+  ART19_REVISION = '1'.freeze
+  VERSION = "3.2.1.#{ART19_REVISION}".freeze
 end
